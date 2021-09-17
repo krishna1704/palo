e48a439ef910f3ec57e347f4dede5449b6e95481
krishna1704 <82215856+krishna1704@users.noreply.github.com>
2021-09-17 16:45:24 -0400

    Update main.tf

--- Example/site to site/main.tf
+++ Example/site to site/main.tf
@@ -1,30 +1,30 @@
 module "vpn-gateway" {
   source  = "https://github.com/krishna1704/palo"
-  version = "1.1.0"
+  version = "1.0.0"
 
   # Resource Group, location, VNet and Subnet details
   # IPSec Site-to-Site connection configuration requirements
-  resource_group_name  = "rg-shared-westeurope-01"
-  virtual_network_name = "vnet-shared-hub-westeurope-001"
-  vpn_gateway_name     = "shared-vpn-gw01"
+  resource_group_name  = ""
+  virtual_network_name = ""
+  vpn_gateway_name     = ""
   gateway_type         = "Vpn"
 
   # local network gateway connection 
   local_networks = [
     {
       local_gw_name         = "onpremise"
-      local_gateway_address = "8.8.8.8"
-      local_address_space   = ["10.1.0.0/24"]
-      shared_key            = "xpCGkHTBQmDvZK9HnLr7DAvH"
+      local_gateway_address = ""
+      local_address_space   = [""]
+      shared_key            = ""
     },
   ]
 
   # Adding TAG's to your Azure resources (Required)
   tags = {
-    ProjectName  = "demo-internal"
-    Env          = "dev"
-    Owner        = "user@example.com"
-    BusinessUnit = "CORP"
-    ServiceClass = "Gold"
+    ProjectName  = ""
+    Env          = ""
+    Owner        = ""
+    BusinessUnit = ""
+    ServiceClass = ""
   }
 }
