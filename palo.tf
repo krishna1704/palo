7a7a083ec7031a6f4a57974a75dee1d921ba935b
krishna1704 <82215856+krishna1704@users.noreply.github.com>
2021-09-21 16:14:05 -0400

    Update palo.tf

--- palo.tf
+++ palo.tf
@@ -1,66 +1,66 @@
 data "panos_system_info" "config" {}
 
-resource "panos_tunnel_interface" "example1" {
-    name = "tunnel.5"
+resource "panos_tunnel_interface" "tunnel" {
+    name = "tunnel.1"
     static_ips = ["10.1.1.1/24"]
-    comment = "Configured for internal traffic"
+    comment = "Azure Site-to-Site VPN"
     vsys    = "virtual system"
 }
-resource "panos_ike_gateway" "example" {
-    name = "example"
-    peer_ip_type = "dynamic"
-    interface = "loopback.42"
+resource "panos_ike_gateway" "IKE" {
+    name = "testvpn"
+    peer_ip_type = "IP"
+    interface = "ethernet1/1"
     pre_shared_key = "secret"
-    local_id_type = "ipaddr"
-    local_id_value = "10.1.1.1"
+    local_id_type = "none"
+    local_id_value = ""
     peer_id_type = "ipaddr"
-    peer_id_value = "10.5.1.1"
-    ikev2_crypto_profile = "myIkeProfile"
-    enable_liveness_check = ""
-    liveness_check_interval = ""
-    enable_passive_mode   = ""
+    peer_id_value = "20.62.109.160"
+    ikev2_crypto_profile = "default"
+    enable_liveness_check = "true"
+    liveness_check_interval = "5"
+    enable_passive_mode   = "false"
 }
-    resource "panos_ike_crypto_profile" "example" {
-    name = "example"
-    dh_groups = ["group1", "group2"]
-    authentications = ["md5", "sha1"]
-    encryptions = ["des"]
+    resource "panos_ike_crypto_profile" "IKE-Crypto" {
+    name = "default"
+    dh_groups = ["group2"]
+    authentications = ["sha256", "sha1"]
+    encryptions = ["3des","aes-128-cdc","aes-256-cdc"]
     lifetime_value = 8
     authentication_multiple = 3
 }
-    resource "panos_ipsec_tunnel" "example" {
-    name = "example"
-    tunnel_interface = "tunnel.7"
-    type    = ""
+    resource "panos_ipsec_tunnel" "IPSec-tunnel" {
+    name = "site-to-site"
+    tunnel_interface = "tunnel.1"
+    type    = "Auto key"
     anti_replay = true
-    ak_ike_gateway = "myIkeGateway"
-    ak_ipsec_crypto_profile = "myIkeProfile"
+    ak_ike_gateway = "testvpn"
+    ak_ipsec_crypto_profile = "test"
 }
 
-    resource "panos_ipsec_crypto_profile" "example" {
-    name = "example"
-    protocol = ""
-    authentications = ["md5", "sha384"]
-    encryptions = ["des", "aes-128-cbc"]
-    dh_group = "group14"
+    resource "panos_ipsec_crypto_profile" "IPSec-Crypto" {
+    name = "test"
+    protocol = "ESP"
+    authentications = ["sha256"]
+    encryptions = ["aes-256-cbc"]
+    dh_group = "no-pfs"
     lifetime_type = "hours"
-    lifetime_value = 4
+    lifetime_value = 1
     lifesize_type = "mb"
     lifesize_value = 1
 }
 
-    resource "panos_static_route_ipv4" "example" {
-    name = "localnet"
-    virtual_router = panos_virtual_router.vr1.name
-    destination = "10.1.7.0/32"
-    interface = ""
-    next_hop = "10.1.7.4"
-    admin_distance = ""
-    metric = ""
-    route_table = ""
-    bfd_profile = ""
+    resource "panos_static_route_ipv4" "Static-Routes" {
+    name = "vpn-traffic"
+    virtual_router = panos_virtual_router.virtual-router.name
+    destination = "10.0.0.0/16"
+    interface = "tunnel.1"
+    next_hop = "none"
+    admin_distance = "10"
+    metric = "10"
+    route_table = "Unicast"
+    bfd_profile = "Disable BFD"
 }
 
-    resource "panos_virtual_router" "vr1" {
-    name = "my virtual router"
+    resource "panos_virtual_router" "virtual-router" {
+    name = "default"
 }
